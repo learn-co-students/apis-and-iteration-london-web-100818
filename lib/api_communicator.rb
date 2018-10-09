011ca47c58672bd901f3e2701fddca6f6bdf2f8e
intuitive-programmer <steven@intuitiveprogrammer.com>
2018-10-09 17:11:44 +0100

    BONUS complete, created helper method

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,11 +2,13 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
+def get_character_movies_from_api
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+end
 
-  response_hash["results"].each { |details|
+def get_films_array(character, character_api)
+  character_api["results"].each { |details|
     if details["name"].downcase == character
       film_list = details["films"].map { |films_api|
         response_hash = JSON.parse(RestClient.get(films_api))
@@ -17,7 +19,6 @@ def get_character_movies_from_api(character)
   }
 end
 
-
 def print_movies(films_array)
   films_array.each_with_index do |film_title, index|
       puts "#{index + 1 }. #{film_title}"
@@ -25,11 +26,7 @@ def print_movies(films_array)
 end
 
 def show_character_movies(character)
-  films_array = get_character_movies_from_api(character)
+  character_api = get_character_movies_from_api
+  films_array = get_films_array(character, character_api)
   print_movies(films_array)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
